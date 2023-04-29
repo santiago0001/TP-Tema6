0d91b73ea9d4ea4837351ad8bcf88cc7a13c2627
sbarzola <sbarzola@arsat.com.ar>
2023-04-29 19:20:07 -0300

    clear principal

--- src/main/java/frgp/utn/edu/ar/Principal.java
+++ src/main/java/frgp/utn/edu/ar/Principal.java
@@ -8,38 +8,11 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistryBuilder;
 
-import frgp.utn.edu.ar.entidad.Autor;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		
-		SessionFactory sessionFactory;
-		Configuration configuration = new Configuration();
-		configuration.configure();
-		
-		ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
-		sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-		
-		Session session = sessionFactory.openSession();
-		
-         session.beginTransaction();
-    	
-    	Autor autor = new Autor();
-    	autor.setNombre("Pepe");
-    	autor.setApellido("123");
-    	autor.setEmail("sdsd");
-    	autor.setNacionalidad("Argentino");
-    	autor.setId("1");
-    	
-    	
-    	
-    	
-    	
-        session.save(autor);  
-    	
-    	session.getTransaction().commit();
-    	session.close();
 		
 	}
 
